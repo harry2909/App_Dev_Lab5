62fbeb18d9831d2c80bd38ea634cad81666f6347
Harry <hpreston2909@gmail.com>
2021-01-30 13:44:39 +0000

    Added javadoc and generated.

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.lab5;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,8 +7,11 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
+    // defining variables
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     public static final String EXTRA_MESSAGE = "com.example.lab4.MainActivity.extra.MESSAGE";
@@ -23,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView mReplyHeadTextView;
     private TextView mReplyTextView;
 
+    /**
+     * OnCreate method is called when the activity starts.
+     *
+     * @param savedInstanceState is used to save the most recently supplied data to the app.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +36,13 @@ public class MainActivity extends AppCompatActivity {
         mReplyHeadTextView = findViewById(R.id.text_header_reply);
         mReplyTextView = findViewById(R.id.text_message_reply);
 
-        //Restore state
-        if (savedInstanceState != null){
+        /*
+        Check if savedInstanceState is not null. Then, if reply is visible, set the text to the
+        reply text.
+         */
+        if (savedInstanceState != null) {
             boolean isVisible = savedInstanceState.getBoolean("reply_visible");
-            if(isVisible){
+            if (isVisible) {
                 mReplyHeadTextView.setVisibility(View.VISIBLE);
                 mReplyTextView.setText(savedInstanceState.getString("reply_text"));
                 mReplyTextView.setVisibility(View.VISIBLE);
@@ -46,52 +54,70 @@ public class MainActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "onCreate");
     }
 
+    // On start, log the message.
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(LOG_TAG, "onStart");
     }
 
+    // On onPause, log the message.
     @Override
     protected void onPause() {
         super.onPause();
         Log.d(LOG_TAG, "onPause");
     }
 
+    // On onRestart, log the message.
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d(LOG_TAG, "onRestart");
     }
 
+    // On onResume, log the message.
     @Override
     protected void onResume() {
         super.onResume();
         Log.d(LOG_TAG, "onResume");
     }
 
+    // On stop, log the message.
     @Override
     protected void onStop() {
         super.onStop();
         Log.d(LOG_TAG, "onStop");
     }
 
+    // On destroy, log the message.
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d(LOG_TAG, "onDestroy");
     }
 
-    // check to see if header is visible then put that state in bundle
+    /**
+     * Saved instance method saves the current data when the app is recreated.
+     * Getting the data from reply text.
+     *
+     * @param outState keyvaluepairs from the app's current state.
+     */
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        if(mReplyHeadTextView.getVisibility() == View.VISIBLE){
+        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
             outState.putBoolean("reply_visible", true);
             outState.putString("reply_text", mReplyTextView.getText().toString());
         }
     }
 
+    /**
+     * Method to launch second activity on button press. Logcat message is set.
+     * Using intent to bundle the message from main activity "EXTRA_MESSAGE".
+     * Start activityforresult is used because we expect a result back.
+     *
+     * @param view used for drawing and handling events.
+     */
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
@@ -100,6 +126,17 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, TEXT_REQUEST);
     }
 
+    /**
+     * This method is used if result is registered. (Data is sent back). Check that the request
+     * code is equal to the one defined.
+     * Check that the result code also equals the same.
+     * Then, using the intent from the result, get the message sent and set it into the
+     * main activity's textview.
+     *
+     * @param requestCode used to check it aligns with the variable defined in mainactivity.
+     * @param resultCode  used to check it aligns with the variable defined in mainactivity.
+     * @param data        the intent data sent from secondactivity.
+     */
     @Override
     public void onActivityResult(int requestCode,
                                  int resultCode, Intent data) {
--- app/src/main/java/com/example/lab5/SecondActivity.java
+++ app/src/main/java/com/example/lab5/SecondActivity.java
@@ -1,7 +1,5 @@
 package com.example.lab5;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,14 +7,22 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class SecondActivity extends AppCompatActivity {
+    //Defining variables
     public static final String EXTRA_REPLY = "com.example.lab4.MainActivity.extra.REPLY";
 
     private static final String LOG_TAG = SecondActivity.class.getSimpleName();
 
     private EditText mReply;
 
+    /**
+     * OnCreate method is called when the activity starts.
+     * Get Intent from MainActivity and then put the message from reply back into it.
+     *
+     * @param savedInstanceState is used to save the most recently supplied data to the app.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +34,12 @@ public class SecondActivity extends AppCompatActivity {
         textView.setText(message);
     }
 
+    /**
+     * Get reply text from the edit text field.
+     * Define new intent, put the reply back as the "EXTRA_REPLY" message.
+     *
+     * @param view used for drawing and event handling.
+     */
     public void returnReply(View view) {
         String reply = mReply.getText().toString();
         Intent replyIntent = new Intent();
